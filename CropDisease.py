f53d4af506b62b30c1d0813d90d9960928734c04
SamTulster <samtulster@gmail.com>
2020-02-13 17:43:23 -0500

    Update Files

--- CropDisease.py
+++ CropDisease.py
@@ -4,7 +4,7 @@ import cv2
 import os
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
-
+from sklearn.model_selection import train_test_split
 fileRoot = "D:\\ICLR-CropDisease\\dataset\\"
 
 
@@ -18,11 +18,11 @@ def loadData():
         res = cv2.resize(img, dsize=(128, 128))
         data_img.append(res)
         data_label.append(0)
-    print(1)
+
     for file in os.listdir(fileRoot + "train\\leaf_rust\\"):
         img = cv2.imread(fileRoot + "train\\leaf_rust\\" + file)
         if img is None:
-            print(file)    # only one file 7U06EV.gif can not be read idk what happened here just ignore it lol
+            print(file)
             continue
         res = cv2.resize(img, dsize=(128, 128))
         data_img.append(res)
@@ -34,11 +34,14 @@ def loadData():
         data_img.append(res)
         data_label.append(2)
 
-    for img in data_img:
-        img = img / 255
+    for i in range(len(data_img)):
+        data_img[i] = data_img[i] / 255
+    data_img = np.array(data_img)
     return data_img, data_label
 
+
 def trainTestSplit(data_img,data_label):
+
     X_train, X_test, y_train, y_test = train_test_split(data_img,data_label,test_size = 0.3)
     return X_train, X_test, y_train, y_test
 
@@ -48,7 +51,8 @@ def trainTestSplit(data_img,data_label):
 def trainModel():
     data_img,data_lable = loadData()
     X_train, X_test, y_train, y_test = trainTestSplit(data_img,data_lable)
-
+    y_train =np.array(y_train)
+    y_test =np.array(y_test)
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -64,12 +68,11 @@ def trainModel():
                   metrics=['accuracy'])
 
     # Train
-    history = model.fit(X_train, y_train, epochs=20,
-                        validation_data=(X_test, y_test))
+    history = model.fit(X_train, y_train, epochs=10)
 
     # test
     test_loss, test_acc = model.evaluate(X_test,y_test, verbose=2)
-    print("test_acc")
+    print(test_acc)
 
 trainModel()
     #output result
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ICLR-CropDisease
 ICLR Computer Vision Compitition
+
 Yunke Zhao, Zhilin Guo
+
 COMS 4995 005 Deep Learning, Spring 2020
